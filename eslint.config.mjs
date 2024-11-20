a81ea3ffef9ef1b3962be01aa3ba714a3395451f
hugtyftg <15224924822@163.com>
2024-11-20 13:21:13 +0800

    feat: update eslint off rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,28 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
 
+const offRules = {
+  '@typescript-eslint/no-explicit-any': 'off',
+  '@typescript-eslint/no-require-imports': 'off',
+};
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommended.map((rulesObj) => {
+    if (rulesObj.name === 'typescript-eslint/recommended') {
+      return {
+        name: 'typescript-eslint/recommended',
+        rules: {
+          ...rulesObj.rules,
+          ...offRules,
+        },
+      };
+    }
+    return rulesObj;
+  }),
   ...pluginVue.configs['flat/essential'],
   {
     files: ['**/*.vue'],
