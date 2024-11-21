1c173eeec11d57094e28a3228e94065665f9442b
hugtyftg <15224924822@163.com>
2024-11-21 17:33:51 +0800

    feat: init vitest of Button component

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,11 +1,28 @@
-<template>
-  <button style="color: red; background-color: #fff;">test button</button>
-</template>
-
-<script setup>
-  // 在 <script setup> 中声明组件选项
-  // https://cn.vuejs.org/api/sfc-script-setup.html#defineoptions
-  defineOptions({
-    name: 'YuButton'
-  })
+<script setup lang="ts">
+import { ref } from "vue";
+import type { ButtonProps } from "./types";
+defineOptions({
+  name: "ErButton",
+});
+const props = withDefaults(defineProps<ButtonProps>(), {
+  tag: 'button',
+  nativeType: 'button'
+})
+const slots = defineSlots();
+const _ref = ref<HTMLButtonElement>();
 </script>
+
+<template>
+  <component :is="tag" ref="_ref" class="er-button" :class="{
+    [`er-button--${type}`]: props.type,
+    [`er-button--${size}`]: props.size,
+    'is-plain': props.plain,
+    'is-round': props.round,
+    'is-circle': props.circle,
+    'is-disabled': props.disabled,
+    'is-loading': props.loading,
+  }" :disabled="props.disabled || props.loading ? true : void 0"
+    :type="props.tag === 'button' ? props.nativeType : void 0">
+    <slot>default slot</slot>
+  </component>
+</template>
