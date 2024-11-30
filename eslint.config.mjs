d579d51ad3530ee4b77d7262b54e101b45245d12
hugtyftg <15224924822@163.com>
2024-11-30 13:46:42 +0800

    feat: update lint for irregular space

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,23 +3,32 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
 
-const offRules = {
+const tsOffRules = {
   '@typescript-eslint/no-explicit-any': 'off',
   '@typescript-eslint/no-require-imports': 'off',
 };
+
+const jsOffRules = {
+  'no-irregular-whitespace': 'off',
+};
+
+const getJsLint = (originRules) => ({
+  rules: Object.assign({}, originRules, jsOffRules),
+});
+console.log();
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
+  getJsLint(pluginJs.configs.recommended.rules),
   ...tseslint.configs.recommended.map((rulesObj) => {
     if (rulesObj.name === 'typescript-eslint/recommended') {
       return {
         name: 'typescript-eslint/recommended',
         rules: {
           ...rulesObj.rules,
-          ...offRules,
+          ...tsOffRules,
         },
       };
     }
