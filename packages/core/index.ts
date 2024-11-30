9bff1f53dda23fb0685c6352d2c776413482c03d
hugtyftg <15224924822@163.com>
2024-11-30 14:25:29 +0800

    feat: import icon package —— fontawesome

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -3,6 +3,11 @@ import components from './components';
 // 全局导入样式
 import '@yu-element/theme/index.css';
 
+// 引入fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
+
 // 注册所有组件的函数
 const installer = install(components);
 
